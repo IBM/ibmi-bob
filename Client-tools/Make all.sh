3f7a818b3d3cdb3d9fcd5e9e199217c7d0fdbe89
Jeff Berman <spamact@yahoo.com>
2017-02-24 00:16:09 -0800

    Improved client-tools scripts.

--- Client-tools/Make all.sh
+++ Client-tools/Make all.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+
+#
 # Kick off a `make all` on the build directory to the target library.
+#
 
+function realpath {
+    local p="$(cd $1 && echo $PWD)"
+    
+    echo "${p}"
+}
+
+argError=0
 localScriptDir="${0%/*}"
-privateKeyDir="/cygdrive/c/Users/$(whoami)/.ssh"  # Change this if not using Cygwin. It should point to user's .ssh directory
 
-# Load in settings specific to this user.
-. ${localScriptDir}/../../my_build_settings.sh
+# Load in settings specific to this user. Path to settings file should have been passed in.
+if (( $# != 0 )); then
+    buildSettings="$1"
+    . ${buildSettings}
+else
+    argError=1
+fi
 
+if (( argError )); then
+    echo 'No build settings file was passed in. Exiting script.'
+    exit ${argError}
+fi
+
+# Issue remote command to build everything
 echo "Source code directory: ${system}:${remoteSourceDir}"
 echo "Target build library: ${buildLib}"
-ssh -i ${privateKeyDir}/id_rsa ${user}@${system} ". /QOpenSys/etc/profile && make all OBJPATH:=/QSYS.LIB/${buildLib}.LIB -f ${remoteSourceDir}/xpmake"
+ssh -i ${privateKey} ${user}@${system} ". /QOpenSys/etc/profile && make all OBJPATH:=/QSYS.LIB/${buildLib}.LIB -f ${remoteSourceDir}/xpmake"
+
+# Update our local Logs directory with the new stuff from the i.
+echo
+echo -n "Copying build logs back to $(realpath ${localSourceDir})."
+rsync -azh -e "ssh -i ${privateKey}" ${user}@${system}:"${remoteSourceDir}/Logs" "${localSourceDir}"
+echo "..Done!"
--- Client-tools/Push to i.sh
+++ Client-tools/Push to i.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
+#
 # Push files to the build directory
+#
 
+function realpath {
+    local p="$(cd $1 && echo $PWD)"
+    
+    echo "${p}"
+}
+
+
+argError=0
 localScriptDir="${0%/*}"
-privateKeyDir="/cygdrive/c/Users/$(whoami)/.ssh"  # Change this if not using Cygwin. It should point to user's .ssh directory
 
-# Load in settings specific to this user.
-. ${localScriptDir}/../../my_build_settings.sh
+# Load in settings specific to this user. Path to settings file should have been passed in.
+if (( $# != 0 )); then
+    buildSettings="$1"
+    . ${buildSettings}
+else
+    argError=1
+fi
 
+if (( argError )); then
+    echo 'No build settings file was passed in. Exiting script.'
+    exit ${argError}
+fi
+
+# Push code to i
 echo "Source directory: $(realpath ${localSourceDir})"
 echo "Target directory: ${system}:${remoteSourceDir}"
-rsync -avzh --exclude .git --exclude .deps --exclude removed --exclude Logs --exclude temp --delete -e "ssh -i ${privateKeyDir}/id_rsa" "${localSourceDir}" ${user}@${system}:"${remoteSourceDir}"
+rsync -avzh --exclude .git --exclude .deps --exclude removed --exclude Logs --exclude temp --exclude .project --delete -e "ssh -i ${privateKey}" "${localSourceDir}" ${user}@${system}:"${remoteSourceDir}"
