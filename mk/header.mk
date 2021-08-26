eaef41375a602619754cf6034a11dd9a43afce46
Tongkun Zhang <tongkun.zhang@ibm.com>
2021-08-26 12:23:48 -0400

    1. Support IBMiEnvCmdList 2. Support running pseudoCode directly 3. Support running SQL Statement 4. Fixed the problem that make keeps rebuilding 5. Add support for more types including DTAs SQLs BNDDs MSGs 6. Make the Rules.mk more compact since we don't specify the path in the target

--- mk/header.mk
+++ mk/header.mk
@@ -1,10 +1,13 @@
 # Clear vars used by this make system
 define HEADER
 CLEAN :=
-TARGETS = $(PFs) $(LFs) $(DSPFs) $(PRTFs) $(CMDs) $(MODULEs) $(SRVPGMs) $(PGMs) $(MENUs) $(PNLGRPs) $(QMQRYs) $(WSCSTs)
+TARGETS = $(DTAs) $(SQLs) $(BNDs) $(PFs) $(LFs) $(DSPFs) $(PRTFs) $(CMDs) $(MODULEs) $(SRVPGMs) $(PGMs) $(MENUs) $(PNLGRPs) $(QMQRYs) $(WSCSTs) $(MSGs)
 SUBDIRS :=
 
 
+DTAs :=
+SQLs :=
+BNDDs :=
 PFs :=
 LFs :=
 DSPFs :=
@@ -17,6 +20,7 @@ MENUs :=
 PNLGRPs :=
 QMQRYs :=
 WSCSTs :=
+MSGs :=
 
 # Clear user vars
 $(foreach v,$(VERB_VARS) $(OBJ_VARS) $(DIR_VARS),$(eval $(v) := ))
