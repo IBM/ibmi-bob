511e5e7526aac3c0685e19aa0b5c947349159842
Tongkun Zhang <tongkun.zhang@ibm.com>
2021-09-20 17:59:14 -0400

    Fix deps

--- mk/def_rules.mk
+++ mk/def_rules.mk
@@ -286,11 +286,7 @@ cleanCDeps = awk '$$2 !~ /^\/QIBM\// && $$2 !~ /$(notdir $<)$$/ && $$2 !~ /$(bas
 # This defines the steps taken after a C compile to massage the auto-generated dependencies into a useable form.
 # See http://make.mad-scientist.net/papers/advanced-auto-dependency-generation/#tldr
 define POSTCCOMPILE =
-$(ICONV) -f $(ICONV_EBCDIC) -t $(ICONV_ASCII) $(DEPDIR)/$(notdir $*).Td | tr -d '\r' > $(DEPDIR)/$(notdir $*).T2d
-$(cleanCDeps) <$(DEPDIR)/$(notdir $*).T2d | sort -u >$(DEPDIR)/$(notdir $*).d
-touch -cr $(OBJPATH)/$@ $(DEPDIR)/$(notdir $*).d
-rm $(DEPDIR)/$(notdir $*).Td $(DEPDIR)/$(notdir $*).T2d
-$(removeEmptyDep)
+
 endef
 
 # cleanRPGDeps removes from the CRTRPGMOD- and CRTSQLRPGI-generated events file any system header files
@@ -500,7 +496,6 @@ programTGTRLS = $(strip \
 
 .SECONDEXPANSION:
 %.CMD: $$(call genDep,$$@,$$*,CMDSRC)
-	$(info $(PGM) $(CMD_PGM) $(*)) 
 	$(eval d = $($@_d))
 	$(call echo_cmd,"=== Creating command [$(notdir $<)]")
 	@$(set_STMF_CCSID)
@@ -593,7 +588,7 @@ programTGTRLS = $(strip \
 
 
 .SECONDEXPANSION:
-%.MODULE: $$(call genDep,$$@,$$*,C) $(DEPDIR)/$$*.d
+%.MODULE: $$(call genDep,$$@,$$*,C)
 	$(eval d = $($@_d))
 	$(call echo_cmd,"=== Creating C module [$(notdir $<)]")
 	@$(set_STMF_CCSID)
@@ -605,7 +600,7 @@ programTGTRLS = $(strip \
 
 
 .SECONDEXPANSION:
-%.MODULE: $$(call genDep,$$@,$$*,RPGLE) $(DEPDIR)/$$*.d
+%.MODULE: $$(call genDep,$$@,$$*,RPGLE)
 	$(eval d = $($@_d))
 	$(call echo_cmd,"=== Creating RPG module [$(notdir $<)]")
 	@$(set_STMF_CCSID)
@@ -629,7 +624,7 @@ programTGTRLS = $(strip \
 
 # Temp: Convert UTF-8 to temporary Windows Latin-1, because SQLC pre-compiler doesn't understand UTF-8
 .SECONDEXPANSION:
-%.MODULE: $$(call genDep,$$@,$$*,SQLC) $(DEPDIR)/$$*.d
+%.MODULE: $$(call genDep,$$@,$$*,SQLC)
 	$(eval d = $($@_d))
 	$(call echo_cmd,"=== Creating SQLC module [$(notdir $<)]")
 	@$(set_STMF_CCSID)
@@ -643,7 +638,7 @@ programTGTRLS = $(strip \
 
 
 .SECONDEXPANSION:
-%.MODULE: $$(call genDep,$$@,$$*,SQLRPGLE) $(DEPDIR)/$$*.d
+%.MODULE: $$(call genDep,$$@,$$*,SQLRPGLE)
 	$(eval d = $($@_d))
 	$(call echo_cmd,"=== Creating SQLRPGLE module [$(notdir $<)]")
 	@$(set_STMF_CCSID)
@@ -840,8 +835,8 @@ programTGTRLS = $(strip \
 # file, we can cause its recipe to normally not run, and we can force its recipe
 # to run by manually `touch`ing its .rebuild file.
 # The following rule causes the initial .rebuild file to be automatically created.
-$(DEPDIR)/%.rebuild:
-	@touch $@
+# $(DEPDIR)/%.rebuild:
+# 	@touch $@
 
 #.PHONY: make_pre
 #make_pre:
