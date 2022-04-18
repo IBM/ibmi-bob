034148141e61bce36ce4377d7356b210d8c8a955
Tongkun Zhang <tongkun.zhang@ibm.com>
2022-04-18 09:00:36 -0400

    simplify unused vars

--- mk/footer.mk
+++ mk/footer.mk
@@ -3,37 +3,11 @@ SUBDIRS_$(d) := $(patsubst %/,%,$(addprefix $(d)/,$(SUBDIRS)))
 
 
 CLEAN_$(d) := $(CLEAN_$(d)) $(filter /%,$(CLEAN) $(TARGETS)) $(addprefix $(d)/,$(filter-out /%,$(CLEAN)))
-
 ifdef TARGETS
-abs_tgts := $(filter /%, $(TARGETS))
-# $(info abs_tgts: $(abs_tgts))
-rel_tgts := $(filter-out /%,$(TARGETS))
-# $(info rel_tgts: $(rel_tgts))
 TARGETS_$(d) := $(TARGETS)
-# $(info $(TARGETS_$(d)))
-
-$(foreach tgt,$(TARGETS),$(eval vpath $(tgt) $(OBJPATH_$(d))))
-# $(info d: $(d); vpath: $(VPATH); objpath: OBJPATH_$(d) $(OBJPATH_$(d)))
-
-
-$(foreach tgt,$(filter-out $(AUTO_TGTS),$(rel_tgts)),$(eval $(call save_vars,$(OBJPATH)/,$(tgt))))
-# Absolute targets are entry points for external (sub)projects which
-# have their own build system - what is really interesting is only CMD
-# and possibly DEPS however I use this general save_vars (two more vars
-# that are not going to be used should not be a problem :P).
-$(foreach tgt,$(abs_tgts),$(eval $(call save_vars,,$(tgt))))
+$(foreach tgt,$(TARGETS),$(eval vpath $(tgt) $(OBJPATH_$(d)))$(eval $(tgt)_d = $(d)))
 endif
 
-# Save user defined vars
-$(foreach v,$(VERB_VARS),$(eval $(v)_$(d) := $($v)))
-$(foreach v,$(OBJ_VARS),$(eval $(v)_$(d) := $(addprefix $(OBJPATH)/,$($v))))
-$(foreach v,$(DIR_VARS),$(eval $(v)_$(d) := $(filter /%,$($v)) $(addprefix $(d)/,$(filter-out /%,$($v)))))
-
-# Update per directory variables that are automatically inherited
-ifeq ($(origin INHERIT_DIR_VARS_$(d)),undefined)
-  INHERIT_DIR_VARS_$(d) := $(or $(INHERIT_DIR_VARS_$(parent_dir)), $(INHERIT_DIR_VARS))
-endif
-$(foreach v,$(INHERIT_DIR_VARS_$(d)),$(if $($(v)_$(d)),,$(eval $(v)_$(d) := $($(v)_$(parent_dir)))))
 
 ########################################################################
 # Inclusion of subdirectories rules - only after this line one can     #
@@ -76,11 +50,6 @@ clean_tree_$(d) : clean_$(d) $(foreach sd,$(SUBDIRS_$(d)),clean_tree_$(sd))
 ifeq ($(filter clean clean_% dist_clean,$(MAKECMDGOALS)),)
 
 SUBDIRS_TGTS := $(foreach sd,$(SUBDIRS_$(d)),$(TARGETS_$(sd)))
-
-# Use the skeleton for the "current dir"
-$(eval $(call skeleton,$(d)))
-# and for each SRCS_VPATH subdirectory of "current dir"
-$(foreach vd,$(SRCS_VPATH),$(eval $(call skeleton,$(d)/$(vd))))
 
 # Target rules for all "non automatic" targets
 # $(foreach tgt,$(filter-out $(AUTO_TGTS),$(TARGETS_$(d))),$(eval $(call tgt_rule,$(tgt))))
