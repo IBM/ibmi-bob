f1acaecd722397602447586e8274aad8087f89fc
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2023-04-15 09:15:15 -0400

    consider variable assignment type

--- tests/data/rules_mks/a.rules.mk
+++ tests/data/rules_mks/a.rules.mk
@@ -2,5 +2,8 @@ SUBDIRS=adir bdir
 
 VAT300.MODULE: vat300.rpgle some.rpgleinc
 VAT300.MODULE: private DFTACTGRP = *NO
-VAT300.MODULE: private TEXT = Andy is cool
-VAT300.MODULE: private DBGVIEW = *LIST
+VAT300.MODULE: private TEXT := Andy is cool
+VAT300.MODULE: private VARSHELL ?= SHELL
+VAT300.MODULE: private VARAPPEND += TOAPPEND
+VAT300.MODULE: private VARIMMED ::= IMMED
+VAT300.MODULE: private VARESCAPE :::= ESCAPE
