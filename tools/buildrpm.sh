a85d79d631548fb02610e5e3d2388d6d34fe199e
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2024-04-18 01:56:39 -0400

    Add script to build the rpm using bash

---
+++ tools/buildrpm.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export VERSION=${GITHUB_REF#refs/tags/v} 
+echo "VERSION ${VERSION}"
+python3 tools/release/generate_spec.py ${VERSION} CHANGELOG True
+cd ~
+mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,BUILDROOT}
+cp bob.spec rpmbuild/SPECS
+PATH=/QOpenSys/pkgs/bin:/QOpenSys/usr/bin:/usr/bin time /QOpenSys/pkgs/bin/rpmbuild -ba --define '_topdir ~/rpmbuild' rpmbuild/SPECS/bob.spec
+export RPM_FILE_PATH=$(find . -name '*.rpm' -print -quit)
+echo "RPM ${RPM_FILE_PATH}"
+mv ${RPM_FILE_PATH } bob.rpm
