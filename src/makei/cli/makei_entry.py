ddb0746173bf9d909821fc5df4c14bff411a4cf1
Irfan Sharif <IrfanSharif@ibm.com>
2025-10-03 16:00:28 -0400

    Fix enforcement of python3.9
    Signed-off-by: Irfan Sharif <IrfanSharif@ibm.com>

--- src/makei/cli/makei_entry.py
+++ src/makei/cli/makei_entry.py
@@ -12,6 +12,9 @@ from makei.build import BuildEnv
 from makei.cvtsrcpf import CvtSrcPf
 from makei.utils import Colors, colored, get_compile_targets_from_filenames
 
+if sys.version_info[:2] != (3, 9):
+    print(f"ERROR: makei requires Python 3.9, but you are running Python {sys.version_info.major}.{sys.version_info.minor}")
+    sys.exit(1)
 
 def cli():
     """
